e6837dcd0c1dd0114329df7cb4bc4fd2f73939ee
wildjcrt <wildjcrt@yahoo.com.tw>
2013-11-02 10:54:03 +0800

    initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SwiftyBackend::Application.config.secret_key_base = 'bfa934ff50da1470195c4fcbe265cfece05bd3655df23db1343dceb8a9cfae85b2189f27e9b0216c99ebf6302a27f4f3bd3dc687dc072ae836ecf825a8467996'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SwiftyBackend::Application.config.session_store :cookie_store, key: '_swifty_backend_session'
