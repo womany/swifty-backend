3751ed82821d8dc06c693a098e8d5f206405c829
wildjcrt <wildjcrt@yahoo.com.tw>
2013-11-02 14:46:40 +0800

    user model, re-migrate columns for omniauth

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 20131102054353) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
