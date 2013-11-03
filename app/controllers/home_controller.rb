76f8b31a4df196cebff982c787f79dc492e33b40
Gary Lai <garylai1990@gmail.com>
2013-11-03 10:00:16 +0800

    Update header name

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
   def index
     @quotes = current_user.quotes.order('id DESC')
+    @tags = @quotes.map {|q| q.tag_list}.flatten.uniq
+  end
+
+  def tag
+    return redirect_to root_path if params[:tag].blank?
+
+    params[:tag] = params[:tag].strip
+    @quotes = current_user.quotes.tagged_with(params[:tag])
+    @tags = @quotes.map {|q| q.tag_list}.flatten.uniq
+    render :index
   end
 end
